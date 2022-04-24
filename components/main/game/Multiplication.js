ccb4d7858e3c5c30d225f6e09cb5ba5bb38b7181
AbrahimMahmud <abrahimmclutch21@gmail.com>
2022-04-24 17:08:58 -0400

    update to add iteration

--- components/main/game/Multiplication.js
+++ components/main/game/Multiplication.js
@@ -20,6 +20,9 @@ export default function Multiplication({ navigation }) {
   var [two, setTwo] = useState(3);
   var [answ, setAnsw] = useState(6);
   var num = parseInt(score);
+  var ques1 = 0;
+  var ques2 = 1; 
+  var quesAns = 2;
 
   function checkAnswer(ans, corAns) {
     if (ans == corAns) {
@@ -31,26 +34,34 @@ export default function Multiplication({ navigation }) {
         .update({
           gameScore: score,
         });
-      newQuestion();
+      nextQuestion();
     } else {
       Alert.alert("Incorrect, try again!", "", [
-        { text: "OK" },
-        { text: "New Question", onPress: () => newQuestion() },
+        { text: "Try Again" },
+        { text: "Next Question", onPress: () => nextQuestion() },
       ]);
     }
   }
   function getRandQuestions() {
-    var randNumOne = Math.floor(Math.random() * (17 - 1) + 1);
-    var randNumTwo = Math.floor(Math.random() * (10 - 1) + 1);
-    var correctAnswer = randNumOne * randNumTwo;
-    return [randNumOne, randNumTwo, correctAnswer];
+    var quesList = [];
+    for (var i = 0; i < 10; i++) {
+      var randNumOne = Math.floor(Math.random() * (17 - 1) + 1);
+      var randNumTwo = Math.floor(Math.random() * (10 - 1) + 1);
+      var correctAnswer = randNumOne * randNumTwo;
+      quesList.push(randNumOne, randNumTwo, correctAnswer);
+    }
+    return quesList;
   }
-  function newQuestion() {
+  function nextQuestion() {
     var newQuestion = getRandQuestions();
-    setOne(newQuestion[0]);
-    setTwo(newQuestion[1]);
-    setAnsw(newQuestion[2]);
+    setOne(newQuestion[ques1]);
+    setTwo(newQuestion[ques2]);
+    setAnsw(newQuestion[quesAns]);
+    ques1 = ques1 + 3;
+    ques2 = ques2 + 3;
+    quesAns = quesAns + 3;
   }
+
   return (
     <SafeAreaView
       style={{
